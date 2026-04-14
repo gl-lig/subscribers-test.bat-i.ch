98af5b24ba4e18072670fd0f4cbfe1c93978a750
gl-lig <gl-lig@pm.me>
2026-04-14 22:42:38 +0200

    fix: features inactives/à zéro masquées en front
    - Seules les features actives sont affichées (plus de grisé)
    - Parcelles: masqué si null et pas illimité
    - Alertes: masqué si 0
    - Stockage: masqué si null/0 et pas illimité
    - Cloud externe: masqué si false
    - Lot de sauvegarde: masqué si false
    - Workspace: affiché si enabled OU illimité (corrige le bug illimité grisé)

--- resources/views/components/subscription-features.blade.php
+++ resources/views/components/subscription-features.blade.php
@@ -4,33 +4,37 @@
     $s = $compact ? 'text-xs' : 'text-sm';
     $ico = $compact ? 'text-sm' : 'text-base';
     $sp = $compact ? 'space-y-2' : 'space-y-3';
-    $logoH = $compact ? 'h-4 w-4' : 'h-5 w-5';
     $icoColor = 'text-gray-300';
-    $icoOff = 'text-gray-200';
 @endphp
 
 <ul class="{{ $sp }}">
+    @if($type->parcelles_unlimited || ($type->parcelles_count && $type->parcelles_count > 0))
     <li class="flex items-center gap-3 {{ $s }}">
         <i class="fa-regular fa-map {{ $ico }} w-5 text-center {{ $icoColor }}"></i>
         <span>{{ __('Parcelles') }} <strong class="text-batid-marine">{{ $type->parcelles_unlimited ? __('Illimité') : $type->parcelles_count }}</strong></span>
     </li>
+    @endif
 
+    @if($type->alertes_count && $type->alertes_count > 0)
     <li class="flex items-center gap-3 {{ $s }}">
         <i class="fa-regular fa-bell {{ $ico }} w-5 text-center {{ $icoColor }}"></i>
         <span>{{ __('Alertes') }} <strong class="text-batid-marine">{{ $type->alertes_count }}</strong></span>
     </li>
+    @endif
 
+    @if($type->stockage_unlimited || ($type->stockage_go && $type->stockage_go > 0))
     <li class="flex items-center gap-3 {{ $s }}">
         <i class="fa-regular fa-hard-drive {{ $ico }} w-5 text-center {{ $icoColor }}"></i>
         <span>{{ __('Stockage') }} <strong class="text-batid-marine">{{ $type->stockage_unlimited ? __('Illimité') : $type->stockage_go . ' Go' }}</strong></span>
     </li>
+    @endif
 
+    @if($type->cloud_externe)
     <li class="{{ $s }}">
-        <div class="flex items-center gap-3 {{ $s }}">
-            <i class="fa-solid fa-cloud {{ $ico }} w-5 text-center {{ $type->cloud_externe ? $icoColor : $icoOff }}"></i>
-            <span class="{{ $type->cloud_externe ? '' : 'text-gray-400' }}">{{ __('Cloud externe') }}</span>
+        <div class="flex items-center gap-3">
+            <i class="fa-solid fa-cloud {{ $ico }} w-5 text-center {{ $icoColor }}"></i>
+            <span>{{ __('Cloud externe') }}</span>
         </div>
-        @if($type->cloud_externe)
         <div style="margin-left:32px;" class="mt-1.5 flex items-center gap-3">
             <img src="https://www.infomaniak.com/favicon.ico" alt="kDrive" style="width:27px;height:27px;" class="flex-shrink-0 rounded-sm object-contain" title="kDrive">
             <svg style="width:27px;height:27px;" class="flex-shrink-0" viewBox="0 0 87.3 78"><title>Google Drive</title><path d="M6.6 66.85 3.3 61.35 29.1 17.1h26.5z" fill="#0066DA"/><path d="M29.1 17.1l26.5 0 28.8 49.75H57.3z" fill="#00AC47"/><path d="M31.2 66.85l-24.6 0L29.1 17.1l26.5 0z" fill="#0066DA"/><path d="M84.4 66.85H31.2L55.6 17.1h0z" fill="#00AC47"/><path d="M6.6 66.85H31.2L29.2 78z" fill="#2684FC"/><path d="M31.2 66.85l53.2 0L58.2 78H29.2z" fill="#FFBA00"/></svg>
@@ -38,23 +42,20 @@
             <svg style="width:27px;height:27px;" class="flex-shrink-0" viewBox="0 0 43.35 40.4"><title>Dropbox</title><path d="M12.87 0L0 8.1l8.8 7.06 12.87-8.4zM0 22.22l12.87 8.1 8.8-7.06-12.87-8.4zM21.67 23.26l8.8 7.06 12.88-8.1-8.81-7.06zM43.35 8.1L30.47 0l-8.8 7.06 12.87 8.1zM21.72 25.16l-8.85 6.98-4.02-2.63v2.95l12.87 7.73 12.87-7.73v-2.95l-4.02 2.63z" fill="#0061FF"/></svg>
             <svg style="width:27px;height:27px;" class="flex-shrink-0" viewBox="0 0 36 36"><title>Proton Drive</title><rect width="36" height="36" rx="8" fill="#6D4AFF"/><path d="M9 10h14.5L18 18l5.5 8H9l5.5-8z" fill="white"/></svg>
         </div>
-        @endif
     </li>
+    @endif
 
+    @if($type->lot_sauvegarde)
     <li class="flex items-center gap-3 {{ $s }}">
-        <i class="fa-solid fa-shield-halved {{ $ico }} w-5 text-center {{ $type->lot_sauvegarde ? $icoColor : $icoOff }}"></i>
-        <span class="{{ $type->lot_sauvegarde ? '' : 'text-gray-400' }}">{{ __('Lot de sauvegarde') }}</span>
+        <i class="fa-solid fa-shield-halved {{ $ico }} w-5 text-center {{ $icoColor }}"></i>
+        <span>{{ __('Lot de sauvegarde') }}</span>
     </li>
+    @endif
 
-    @if($type->workspace_enabled)
+    @if($type->workspace_enabled || $type->workspace_unlimited)
     <li class="flex items-center gap-3 {{ $s }}">
         <i class="fa-solid fa-people-group {{ $ico }} w-5 text-center {{ $icoColor }}"></i>
         <span>{{ __('Workspace') }} <strong class="text-batid-marine">{{ $type->workspace_unlimited ? __('Illimité') : $type->workspace_count }}</strong></span>
-    </li>
-    @else
-    <li class="flex items-center gap-3 {{ $s }}">
-        <i class="fa-solid fa-people-group {{ $ico }} w-5 text-center {{ $icoOff }}"></i>
-        <span class="text-gray-400">{{ __('Workspace') }}</span>
     </li>
     @endif
 </ul>
